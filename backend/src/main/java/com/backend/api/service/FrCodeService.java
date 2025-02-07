c3712cdd08dd31ecbf80cfeac2d1b62c20c354a2
Aaron Blakely <briancole.dev@gmail.com>
2025-02-07 08:30:02 -0800

    removed backend log

--- backend/src/main/java/com/backend/api/service/FrCodeService.java
+++ backend/src/main/java/com/backend/api/service/FrCodeService.java
@@ -19,21 +19,15 @@ public class FrCodeService {
         Map<String, List<Map<String, String>>> dropdowns = new HashMap<>();
         
         // Fetch from database
-        log.info("Fetching countries...");
         List<Map<String, String>> countries = frCodeRepository.findCodeAndDescriptionByCodeName("frcodeCountry");
-        log.info("Found {} countries", countries.size());
         dropdowns.put("countries", countries);
 
-        log.info("Fetching states...");
         List<Map<String, String>> states = frCodeRepository.findCodeAndDescriptionByCodeName("frcodeState");
-        log.info("Found {} states", states.size());
         dropdowns.put("states", states);
 
-        log.info("Fetching provinces...");
         List<Map<String, String>> provinces = frCodeRepository.findCodeAndDescriptionByCodeName("frcodeProvince");
-        log.info("Found {} provinces", provinces.size());
         dropdowns.put("provinces", provinces);
         
         return dropdowns;
     }
-} 
+}
